e00a72dac921e7e0ba5cea6adae7d1c787c3b5e3
SergioPazzoInCulo <88167244+SergioPazzoInCulo@users.noreply.github.com>
2021-07-30 00:13:13 +0200

    Create GeneticCode.hs

---
+++ GeneticCode.hs
@@ -0,0 +1,30 @@
+Republic Of Rabbits
+_____________________
+  
+Ministry of Interior
+
+
+                     /\    .-" /
+                    /  ; .'  .' 
+                   :   :/  .'   
+                    \  ;-.'     
+       .--""""--..__/     `.    
+     .'           .'    `o  \   
+    /                    `   ;  
+   :                  \      :  
+ .-;        -.         `.__.-'  
+:  ;          \     ,   ;       
+'._:           ;   :   (        
+    \/  .__    ;    \   `-.     
+    ;     "-,/_..--"`-..__)    
+     '""--.._:
+
+Name = Sergio
+Birthday = 10/03/2021
+Date of Adoption = 25/04/2021
+Race = Lion Head
+Birth place = Rimini (RN)
+Citizenship = bnui
+Height = idk
+Special signs = Black spots all over the body and face.
+Other = Crazy in the ass
